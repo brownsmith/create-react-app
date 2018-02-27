313a50d12520578f0367e0bed904cbcd6a18d376
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-02-27 08:08:12 +0000

    test component file

---
+++ my-app/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import MyComponent from 'components/MyComponent';
+
+export default class MyComponent extends Component {
+  render() {
+    return <div className="test">Hellos</div>;
+  }
+}
